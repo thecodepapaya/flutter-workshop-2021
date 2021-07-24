52d6015ad76d037ac56db3d631166b990ac1f8ea
Ashutosh Singh <thecodepapaya@gmail.com>
2021-07-24 17:03:42 +0530

    📦 Add pub package

---
+++ pub_package/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Tap the button to show a toast"),
+            SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                Fluttertoast.showToast(msg: "Hi there! I'm a toast.");
+              },
+              child: Text("Press me"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
